26abfce5e22a9c6967e88ef7efdf57cd58ec28e1
lazizB17 <abdulazizboriyev993@gmail.com>
2022-06-05 21:44:48 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/PDP/pdpui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.PDP.pdpui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/pdp_ui.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class PDP extends StatefulWidget {
+  static const id = '/pdp_ui';
+  
+  const PDP({Key? key}) : super(key: key);
+
+  @override
+  State<PDP> createState() => _PDPState();
+}
+
+class _PDPState extends State<PDP> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("UI", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400)),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 40,
+              width: 75,
+              child: const Text("User", style: TextStyle(color: Colors.red, fontSize: 30),),
+            ),
+            Container(
+              height: 40,
+              width: 135,
+              child: const Text("Interface", style: TextStyle(color: Colors.green, fontSize: 30),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
